c953f08d4eec066fd74a22f896e523a010b65727
kdw1437 <kdw1437@naver.com>
2024-04-04 00:32:16 +0900

    settlement date quote

--- PostQuoteData.java
+++ PostQuoteData.java
@@ -27,40 +27,38 @@ public class PostQuoteData {
         
         try {
             JSONArray jsonArray = new JSONArray(jsonStr);
-            String[] columns = {"1stredemday", "2ndredemday", "3rdredemday", "4thredemday", "5thredemday", "6thredemday"};
-            ListParam listParam = new ListParam(columns);
+            String[] columns = {
+                    "1stredemday", "2ndredemday", "3rdredemday", "4thredemday", "5thredemday", "6thredemday",
+                    "1stsettlement", "2ndsettlement", "3rdsettlement", "4thsettlement", "5thsettlement", "6thsettlement"
+                };
+                ListParam listParam = new ListParam(columns);
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObj = jsonArray.getJSONObject(i);
-                String effectiveDateStr = jsonObj.getString("effectiveDate");
-                int earlyRedempCycleMonths = jsonObj.getInt("earlyRedempCycle");
-                int settleDateOffset = jsonObj.getInt("settleDateOffset");
-                
-                LocalDate effectiveDate = LocalDate.parse(effectiveDateStr, DateTimeFormatter.ofPattern("yyyyMMdd"));
-                List<String> dates = calculateFutureDates(effectiveDate, earlyRedempCycleMonths);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObj = jsonArray.getJSONObject(i);
+                    String effectiveDateStr = jsonObj.getString("effectiveDate");
+                    int earlyRedempCycleMonths = jsonObj.getInt("earlyRedempCycle");
+                    int settleDateOffset = jsonObj.getInt("settleDateOffset");
 
-                // 새로운 열을 생성한다.
-                int rowIndex = listParam.createRow();
+                    LocalDate effectiveDate = LocalDate.parse(effectiveDateStr, DateTimeFormatter.ofPattern("yyyyMMdd"));
+                    List<String> dates = calculateFutureDates(effectiveDate, earlyRedempCycleMonths);
+                    List<String> settlementDates = calculateSettlementDates(dates, settleDateOffset);
 
-                // 예상대로 dates list가 정확히 6개의 element를 가지는지 확인한다.
-                if (dates.size() != columns.length) {
-                    throw new IllegalArgumentException("The dates list must contain exactly 6 elements.");
+                    // listParam 새로운 열을 생성
+                    int rowIndex = listParam.createRow();
+
+                    // 각 column에 맞게 date를 할당
+                    for (int j = 0; j < 6; j++) {
+                        listParam.setValue(rowIndex, columns[j], dates.get(j));
+                        listParam.setValue(rowIndex, columns[6 + j], settlementDates.get(j));
+                    }
+
+                    log.debug("Calculated future dates for JSON object {}: {}", i, String.join(", ", dates));
+                    log.debug("Calculated settlement dates for JSON object {}: {}", i, String.join(", ", settlementDates));
                 }
 
-                // 리스트로 부터 각각의 date를 각 column에 할당한다.
-                for (int j = 0; j < columns.length; j++) {
-                    listParam.setValue(rowIndex, columns[j], dates.get(j));
-                }
-
-                String datesString = String.join(", ", dates);
-                log.debug("Calculated future dates for JSON object {}: {}", i, datesString);
-            }
-
-            // 여러 row를 가진 listParam 객체를 사용한다.
-            // listParam객체를 daoService의 setValue method를 통해서 데이터베이스의 테이블에 넣을 수 있다.
-            log.debug("Final ListParam with multiple rows: {}", listParam.toString());
+                log.debug("Final ListParam with multiple rows: {}", listParam.toString());
             
-            log.debug("Final ListParam with multiple rows: {}", listParam.toString());
+                log.debug("Final ListParam with multiple rows: {}", listParam.toString());
         } catch (Exception e) {
         	log.error("Error executing PostQuoteData", e);
         }
@@ -78,6 +76,17 @@ public class PostQuoteData {
     	return dates;
     }
     
+    private List<String> calculateSettlementDates(List<String> futureDates, int settleDateOffset) {
+        List<String> settlementDates = new ArrayList<>();
+        for (String dateStr : futureDates) {
+            LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyyMMdd"));
+            date = date.plusDays(settleDateOffset);
+            date = adjustForWeekendAndHolidays(date);
+            settlementDates.add(date.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        }
+        return settlementDates;
+    }
+
     private LocalDate adjustForWeekendAndHolidays(LocalDate date) {
     	//date가 holiday인지 확인하고, 날짜를 조정해준다.
     	while (date.getDayOfWeek() == java.time.DayOfWeek.SATURDAY ||
