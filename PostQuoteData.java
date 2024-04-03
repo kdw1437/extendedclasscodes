314d0147cf83d74eb889cd4a9cb99759267f83a1
kdw1437 <kdw1437@naver.com>
2024-04-03 18:35:25 +0900

    PostQuoteData_Date

---
+++ PostQuoteData.java
@@ -0,0 +1,75 @@
+package com.jurosys.extension.com;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+
+import com.uro.DaoService;
+import com.uro.log.LoggerMg;
+import com.uro.service.sql.SQLServiceException;
+import com.uro.transfer.ListParam;
+import com.uro.transfer.ParamException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostQuoteData {
+    Logger log = LoggerMg.getInstance().getLogger();
+
+    public void execute(DaoService dao) {
+        String dataSetId = dao.getRequest().getParameter("dataSetId");
+        String baseDt = dao.getRequest().getParameter("baseDt");
+        //jsonStr이 DaoService 객체에서 받은 JSON string이다.
+        String jsonStr = dao.getStringValue("a");
+        
+        try {
+        	JSONArray jsonArray = new JSONArray(jsonStr);
+        	for (int i = 0; i <jsonArray.length(); i++) {
+        		JSONObject jsonObj = jsonArray.getJSONObject(i);
+        		String effectiveDateStr = jsonObj.getString("effectiveDate");
+        		int earlyRedempCycleMonths = jsonObj.getInt("earlyRedempCycle");
+        		
+        		LocalDate effectiveDate = LocalDate.parse(effectiveDateStr, DateTimeFormatter.ofPattern("yyyyMMdd"));
+        	
+        		List<String> dates = calculateFutureDates(effectiveDate, earlyRedempCycleMonths);
+        		
+        		String datesString = String.join(", ", dates);
+        		log.info("Calculated future dates: {}", datesString);
+        		
+        		log.info("Calculated future dates: {}", datesString);
+
+        	}
+        } catch (Exception e) {
+        	log.error("Error executing PostQuoteData", e);
+        }
+        
+    }
+    
+    private List<String> calculateFutureDates(LocalDate startDate, int monthsBetween) {
+    	List<String> dates = new ArrayList<>();
+    	LocalDate date = startDate;
+    	for (int i = 0; i < 6; i++) { //6개의 element가 arrayList에 들어간다.
+    		date = date.plusMonths(monthsBetween);
+    		date = adjustForWeekendAndHolidays(date);
+    		dates.add(date.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+    	}
+    	return dates;
+    }
+    
+    private LocalDate adjustForWeekendAndHolidays(LocalDate date) {
+    	//date가 holiday인지 확인하고, 날짜를 조정해준다.
+    	while (date.getDayOfWeek() == java.time.DayOfWeek.SATURDAY ||
+    			date.getDayOfWeek() == java.time.DayOfWeek.SUNDAY ||
+    			isHoliday(date)) {
+    		date = date.plusDays(1);
+    	}
+    	return date;
+    }
+    
+    private boolean isHoliday(LocalDate date) {
+    	return false;
+    }
+}
